ba9de5af477282ced1c56542811110bc8c4e89c4
Sayani Sen <sayanisen.official@gmail.com>
2025-01-28 13:04:47 +0530

    Two pointers

--- TwoPointers/palindrome.js
+++ TwoPointers/palindrome.js
@@ -2,3 +2,33 @@
 Given a string s, return true if it is a palindrome, otherwise return false.
 A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
 */
+let s = "Was it a car or a cat I saw?";
+
+const isPalindrome = (s) => {
+  let l = 0;
+  let r = s.length - 1;
+
+  while (l < r) {
+    while (l < r && !alphaNum(s[l])) {
+      l++;
+    }
+    while (r > l && !alphaNum(s[r])) {
+      r--;
+    }
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
+};
+
+function alphaNum(c) {
+  return (
+    (c >= "A" && c <= "Z") || (c >= "a" && c <= "z") || (c >= "0" && c <= "9")
+  );
+}
+
+const result = isPalindrome(s);
+console.log(result);
