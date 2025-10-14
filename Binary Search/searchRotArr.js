40b7aee3e8b0288069d2f1ddd5cf0a5b54fe2cb0
Sayani Sen <sayanisen.official@gmail.com>
2025-10-14 11:57:39 +0530

    binarysearch

---
+++ Binary Search/searchRotArr.js
@@ -0,0 +1,45 @@
+/*
+leet 33. Search in Rotated Sorted Array
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+ */
+class Solution {
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            const m = Math.floor((l + r) / 2);
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+
+        const pivot = l;
+
+        const result = this.binarySearch(nums, target, 0, pivot - 1);
+        if (result !== -1) {
+            return result;
+        }
+
+        return this.binarySearch(nums, target, pivot, nums.length - 1);
+    }
+
+    binarySearch(nums, target, left, right) {
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
